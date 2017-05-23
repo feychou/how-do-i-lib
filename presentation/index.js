43d157fcafe9bb540564ae559b1071c5f85f4a1f
feychou <federica.recanatini@gmail.com>
2017-05-23 14:02:08 +0200

    Add some slides

--- presentation/index.js
+++ presentation/index.js
@@ -48,43 +48,43 @@ export default class Presentation extends React.Component {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary" style={{position:"relative"}}>
-          <Heading size={1} fit caps lineHeight={1} textColor="quartenary">
+          <Heading size={1} fit caps lineHeight={1} textColor="tertiary">
             Javacript: how do I even lib?
           </Heading>
           <Heading size={1} fit caps lineHeight={1} textColor="secondary" style={{position:"absolute", top:"44px", left:"37%"}}>
             Javacript: how do I even lib?
           </Heading>
+          <Text textColor="tertiary" size={2} margin="10px 0 0" bold fit style={{position:"absolute", top:"90px", left:"29%"}}>Creating and consuming third party libs in Javacript</Text>
           <img src="https://avatars3.githubusercontent.com/u/1609297?v=3&s=460" width="200" height="200" style={{float:'left', margin:'10px 0 0'}} />
-          <Text margin="80px 0 0" textColor="tertiary" size={2} bold>
+          <Text margin="168px 0 0" textColor="tertiary" size={6}>
             https://feychou.github.io/
           </Text>
         </Slide>
 
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+          <Heading size={3} textColor="secondary">FOR EVERYONE!</Heading>
+          <Text size={6} textColor="secondary">People who have never done!</Text>
+          <Text size={6} textColor="secondary">People who did it and want to be reminded why they hated it!</Text>
+          <img src="http://www.stickermotions.com/wp-content/uploads/2015/04/88831530358093871666.png" />
         </Slide>
 
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="tertiary">
+          <Heading size={3} textColor="primary" caps>Why?</Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Disregarded topic</ListItem>
+            <ListItem>Partial information available</ListItem>
+            <ListItem>Open source relevance</ListItem>
+            <ListItem>Technical benefits (better codebases)</ListItem>
           </List>
         </Slide>
 
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+        <Slide transition={["fade"]} bgColor="primary" textColor="quartenary">
+          <Heading size={3} textColor="secondary" caps>Where to start?</Heading>
+          <List>
+            <ListItem>Scaffolder?</ListItem>
+            <ListItem>Consider the responsibility</ListItem>
+            <ListItem>Understand how to distribute it</ListItem>
+          </List>
         </Slide>
       </Deck>
     );
